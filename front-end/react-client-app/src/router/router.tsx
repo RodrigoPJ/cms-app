33ca3bd8758c0ce3a4130605a1fdd9581a351cb9
Rod P <rodrigo.igeofisica@gmail.com>
2025-06-17 13:01:17 -0700

    rpena: hardcoded basename

--- front-end/react-client-app/src/router/router.tsx
+++ front-end/react-client-app/src/router/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import routes from "./routes.tsx";
 
-const BE = import.meta.env;
-const githubBuild = BE["VITE_BUILD_TARGET"];
+// const BE = import.meta.env;
+// const githubBuild = BE["VITE_BUILD_TARGET"];
 
 const router = createBrowserRouter(routes, {
-  basename: githubBuild ? "/cms-app/" : undefined,
+  basename: "/cms-app/"
 });
 
 export default router;
