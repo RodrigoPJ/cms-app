a7ab2c2b729960ec0a5127cfd1873bcf7b04aa3f
Rod P <rodrigo.igeofisica@gmail.com>
2025-06-17 13:51:13 -0700

    rpena: env var for baseneme

--- front-end/react-client-app/src/router/router.tsx
+++ front-end/react-client-app/src/router/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import routes from "./routes.tsx";
 
-// const BE = import.meta.env;
-// const githubBuild = BE["VITE_BUILD_TARGET"];
+const BE = import.meta.env;
+const githubBuild = BE["VITE_BUILD_TARGET"];
 
 const router = createBrowserRouter(routes, {
-  basename: "/cms-app/"
+  basename: githubBuild ? "/cms-app/" : undefined,
 });
 
 export default router;
